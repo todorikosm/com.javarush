9e6578fd946070f15ad2e4c024bca57e565e0a8d
stodoriko <s.todorikos@gmail.com>
2025-10-07 18:08:08 +0300

    cycles -- task2

---
+++ src/main/java/mod1/lev4_Cycles/task2/Solution.java
@@ -0,0 +1,17 @@
+package mod1.lev4_Cicles.task2;
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 5;
+
+        while(i > 0) {
+            int k = 10;
+            while(k > 0) {
+                System.out.print("Q");
+                k--;
+            }
+            System.out.println(" ");
+            i--;
+        }
+    }
+}
