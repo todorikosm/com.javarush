9ba71c906be30de815952829d969b964ec713d07
stodoriko <s.todorikos@gmail.com>
2025-10-08 16:05:21 +0300

    lev4_cycles -- task10

---
+++ src/main/java/mod1/lev4_Cycles/task10/Solution.java
@@ -0,0 +1,21 @@
+package mod1.lev4_Cycles.task10;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        double glass = 0.5;
+
+        Scanner console = new Scanner(System.in);
+        boolean value = console.nextBoolean();
+
+        if (value) {
+            int result = (int)Math.ceil(glass);
+            System.out.println(result);
+        } else if (!value) {
+            int result = (int)Math.floor(glass);
+            System.out.println(result);
+        }
+
+    }
+}
