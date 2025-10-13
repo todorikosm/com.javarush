934ddce28514d82ce24e63e3e12f3678bb9ffe11
stodoriko <s.todorikos@gmail.com>
2025-10-13 20:59:35 +0300

    lev5_types -- task 1,2,3

---
+++ src/main/java/mod1/lev8_Types/task1/Solution.java
@@ -0,0 +1,18 @@
+package mod1.lev8_Types.task1;
+
+/*
+В методе main вызови метод div() 2 раза. Первый раз — с такими аргументами,
+чтобы вывод в консоли был "Infinity", а второй — "-Infinity".
+Код метода div() изменять нельзя.
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        div( 0.0, 10);
+        div( 0.0, -10);
+    }
+
+    public static void div(double a, double b){
+        System.out.println(b/a);
+    }
+}
