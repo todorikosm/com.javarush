f5d0ec95ace84d34838d6113de5e0afab0f319ba
stodoriko <s.todorikos@gmail.com>
2025-10-14 17:10:32 +0300

    lev9 -- task 1

---
+++ src/main/java/mod1/lev9/task1/Solution.java
@@ -0,0 +1,23 @@
+package mod1.lev9.task1;
+
+/*
+Реализуй методы sin(double), cos(double), tan(double) которые возвращают синус, косинус и тангенс угла соответственно,
+полученного как параметр. Угол задан в градусах.
+В этом тебе помогут соответствующие методы класса Math, которые принимают параметром угол, заданный в радианах.
+ */
+
+import static java.lang.Math.toRadians;
+
+public class Solution {
+    public static double sin(double a) {
+        return Math.sin(toRadians(a));
+    }
+
+    public static double cos(double a) {
+        return Math.cos(toRadians(a));
+    }
+
+    public static double tan(double a) {
+        return Math.tan(toRadians(a));
+    }
+}
