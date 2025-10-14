5298f8d22f870aed11b51b4cd2d4d9087a013b9f
stodoriko <s.todorikos@gmail.com>
2025-10-14 19:20:29 +0300

    lev9 -- task 4

---
+++ src/main/java/mod1/lev9/task4/Solution.java
@@ -0,0 +1,23 @@
+package mod1.lev9.task4;
+
+/*
+псевдослучайные числа
+ */
+
+public class Solution {
+    public static int a = 41;
+    public static int c = 11119;
+    public static int m = 11113;
+    public static int seed = 1;
+
+    public static int getNextRandom() {
+        seed = (a * seed + c) % m;
+        return seed;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(getNextRandom());
+        }
+    }
+}
