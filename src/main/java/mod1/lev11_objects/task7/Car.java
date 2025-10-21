1ab330c8b6e7e7616682565d63377aa636affdc1
stodoriko <s.todorikos@gmail.com>
2025-10-21 17:42:43 +0300

    lev11 -- task 7

---
+++ src/main/java/mod1/lev11_objects/task7/Car.java
@@ -0,0 +1,15 @@
+package mod1.lev11_objects.task7;
+
+/*
+Напиши свою реализацию hashCode, используя переменные model и year. Если эти поля у двух объектов одинаковые,
+то должен возвращаться одинаковый hashCode. Если правильно реализовать метод hashCode, вывод должен быть таким:
+true
+true
+true
+true
+false
+false
+ */
+
+public class Solution {
+}
