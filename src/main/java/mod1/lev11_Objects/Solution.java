7f36180b001c0219f56135f3497be7569ad69c4c
stodoriko <s.todorikos@gmail.com>
2025-10-21 15:37:26 +0300

    lev11 -- task 5

---
+++ src/main/java/mod1/lev11_Objects/Solution.java
@@ -0,0 +1,28 @@
+package mod1.lev11_Objects;
+
+/*
+В классе Solution есть поле currentYear, которое должно инициализироваться значением текущего
+года(Calendar.getInstance().get(Calendar.YEAR)) при создании нового объекта типа Solution.
+Тебе нужно найти ошибку и исправить её, чтобы при создании объекта класса Solution поле currentYear
+правильно инициализировалось.
+ */
+
+import java.util.Calendar;
+
+public class Solution {
+    private int currentYear;
+
+    public void setCurrentYear() {
+        this.currentYear = Calendar.getInstance().get(Calendar.YEAR);
+    }
+
+    public int getCurrentYear() {
+        return currentYear;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.setCurrentYear();
+        System.out.println(solution.getCurrentYear());
+    }
+}
