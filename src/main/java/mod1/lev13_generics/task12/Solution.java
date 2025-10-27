71e2ab9c4e3b297b64a2f356c54acc74a419f209
stodoriko <s.todorikos@gmail.com>
2025-10-27 21:50:32 +0300

    lev13 -- task 11,12

---
+++ src/main/java/mod1/lev13_generics/task12/Solution.java
@@ -0,0 +1,16 @@
+package mod1.lev13_generics.task12;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>() {{
+                add("Так");
+                add("тоже");
+                add("можно");
+                add("делать");
+                add("!");
+        }};
+
+    }
+}
