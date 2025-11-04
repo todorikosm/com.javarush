90d13314f156899da17de1645d436782d97d6cb8
stodoriko <s.todorikos@gmail.com>
2025-11-04 14:34:32 +0300

    lev14 -- task 16,17

---
+++ src/main/java/mod1/lev14_collections/task17/Month.java
@@ -0,0 +1,22 @@
+package mod1.lev14_collections.task17;
+
+/*
+В классе Solution реализуй метод getNextMonth, который должен возвращать следующий месяц,
+а если передан последний месяц, возвращать первый. Используй методы values и ordinal.
+Метод main не участвует в проверке.
+ */
+
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
---
+++ src/main/java/mod1/lev14_collections/task17/Solution.java
@@ -0,0 +1,13 @@
+package mod1.lev14_collections.task17;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getNextMonth(Month.JANUARY));
+        System.out.println(getNextMonth(Month.JULY));
+    }
+
+    public static Month getNextMonth(Month month) {
+        int ordinal = month == Month.DECEMBER ? 0 : month.ordinal() + 1;
+        return Month.values()[ordinal];
+    }
+}
