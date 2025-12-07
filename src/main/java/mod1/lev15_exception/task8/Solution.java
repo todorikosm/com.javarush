ed8e2c3de91646b8585422737b7d437a261b6c64
stodoriko <s.todorikos@gmail.com>
2025-12-07 23:45:42 +0300

    lev15 -- task 8-10

---
+++ src/main/java/mod1/lev15_exception/task8/Solution.java
@@ -0,0 +1,27 @@
+package mod1.lev15_exception.task8;
+
+/*
+Перехвати исключение в методе main, чтобы программа компилировалась.
+При возникновении исключения нужно вывести на экран строку errorMessage.
+ */
+
+public class Solution {
+
+    public static String errorMessage = "не повезло";
+
+    public static void main(String[] args) {
+        try {
+            generateLuckyNumber();
+        } catch (Exception e) {
+            System.out.println(errorMessage);
+        }
+    }
+
+    static void generateLuckyNumber() throws Exception {
+        int luckyNumber = (int) (Math.random() * 100);
+        if (luckyNumber == 13) {
+            throw new Exception();
+        }
+        System.out.println("твое счастливое число: " + luckyNumber);
+    }
+}
