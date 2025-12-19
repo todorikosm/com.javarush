b7ec9ffab35903194804f82f8798466ecc29f39a
stodoriko <s.todorikos@gmail.com>
2025-12-20 00:22:45 +0300

    lev16 -- task 3

--- src/main/java/mod1/lev16_try_with_resources/task3/Solution.java
+++ src/main/java/mod1/lev16_try_with_resources/task3/Solution.java
@@ -1,4 +1,28 @@
 package mod1.lev16_try_with_resources.task3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/*
+Программа считывает с консоли путь к файлу, читает строки из файла и выводит их на экран.
+Перепиши код программы с использованием try-with-resources: вынеси в круглые скобки создание
+объектов с внешними ресурсами. Не забудь удалить ненужный блок finally и вызовы метода close().
+*/
+
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        try(Scanner scanner = new Scanner(System.in);
+            BufferedReader bufferedReader = Files.newBufferedReader(Path.of(scanner.nextLine())))
+        {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Something went wrong : " + e);
+        }
+    }
 }
