0f7d26afc1c0b53e8096a11256b79118abbcca70
todorikosm <s.todoriko@yandex.ru>
2026-02-16 00:26:32 +0300

    lev14 task18

---
+++ src/main/java/mod1/lev14_collections/task18/Month.java
@@ -0,0 +1,40 @@
+package mod1.lev14_collections.task18;
+
+/*
+В enum Month добавь 4 метода: getWinterMonths(), getSpringMonths(), getSummerMonths(), getAutumnMonths()
+которые будут возвращать массив с тремя месяцами.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER;
+
+    public static Month[] getWinterMonths() {
+        return new Month[]{DECEMBER, JANUARY, FEBRUARY};
+    }
+
+    public static Month[] getSpringMonths() {
+        return new Month[]{MARCH, APRIL, MAY};
+    }
+
+    public static Month[] getSummerMonths() {
+        return new Month[]{JUNE, JULY, AUGUST};
+    }
+
+    public static Month[] getAutumnMonths() {
+        return new Month[]{SEPTEMBER, OCTOBER, NOVEMBER};
+    }
+}
