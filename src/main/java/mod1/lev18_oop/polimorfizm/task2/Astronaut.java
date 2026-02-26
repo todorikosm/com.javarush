3e503fdf2da7e9f4d3f52aab8fd2008b1f819012
todorikosm <s.todoriko@yandex.ru>
2026-02-26 17:51:09 +0300

    oop -- polimorfizm -- task2

---
+++ src/main/java/mod1/lev18_oop/polimorfizm/task2/Astronaut.java
@@ -0,0 +1,7 @@
+package mod1.lev18_oop.polimorfizm.task2;
+
+public class Astronaut {
+    public String getInfo() {
+        return "Неизвестный астронавт";
+    }
+}
---
+++ src/main/java/mod1/lev18_oop/polimorfizm/task2/Solution.java
@@ -0,0 +1,52 @@
+package mod1.lev18_oop.polimorfizm.task2;
+
+import java.util.ArrayList;
+
+
+public class Solution {
+    public static ArrayList<Astronaut> astronauts = new ArrayList<>();
+
+    public static void main(String[] args) {
+        createCrew();
+        printCrewInfo();
+        runWorkingProcess();
+    }
+
+    public static void runWorkingProcess() {
+        for (Astronaut el : astronauts) {
+            if (el instanceof Human) {
+                pilot((Human) el);
+            } else if (el instanceof Dog) {
+                createDirection((Dog) el);
+            } else if (el instanceof Cat) {
+                research((Cat) el);
+            }
+        }
+    }
+
+    public static void pilot(Human human){
+        System.out.println("Член экипажа " + human.getInfo() + " пилотирует корабль.");
+    }
+
+    public static void createDirection(Dog dog){
+        System.out.println("Член экипажа " + dog.getInfo() + " занимается созданием навигационного маршрута.");
+    }
+
+    public static void research(Cat cat){
+        System.out.println("Член экипажа " + cat.getInfo() + " исследует ближайшие планеты.");
+    }
+
+    public static void createCrew() {
+        astronauts.add(new Human());
+        astronauts.add(new Human());
+        astronauts.add(new Dog());
+        astronauts.add(new Cat());
+    }
+
+    public static void printCrewInfo() {
+        System.out.println("На борт погружены члены экипажа: ");
+        for (Astronaut astronaut : astronauts) {
+            System.out.println(astronaut.getInfo());
+        }
+    }
+}
